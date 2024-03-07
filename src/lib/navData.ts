ff43172862010814b4d3780808f0597083bf0a00
JojoDeveloper01 <jojo.programmer01@gmail.com>
2024-03-07 23:35:56 +0000

    fix sections dropdowns

---
+++ src/lib/navData.ts
@@ -0,0 +1,58 @@
+const sections = [
+  {
+    name: 'Início',
+    path: '/',
+  },
+  {
+    name: 'Sobre Nós',
+    path: '/sobrenos',
+  },
+  {
+    name: 'Contactos',
+    path: '/contactos',
+  },
+  {
+    name: 'Serviços',
+    path: '/servicos',
+  },
+  {
+    name: 'Perguntas',
+    path: '/#perguntas',
+  },
+  {
+    name: 'Testemunhas',
+    path: '/#testemunhas',
+  },
+]
+
+const dropdownServicos = [
+  {
+    name: 'Micropigmentação Sobrancelhas',
+    path: '/servicos/micropigmentacao-sobrancelhas',
+  },
+  {
+    name: 'Micropigmentação Labial',
+    path: '/servicos/micropigmentacao-labial',
+  },
+  {
+    name: 'Micropigmentação Olhos',
+    path: '/servicos/micropigmentacao-olhos',
+  },
+]
+
+const dropdownPerguntas = [
+  {
+    name: 'O que é micropigmentação?',
+    path: '/#pg1',
+  },
+  {
+    name: 'A micropigmentação dói?',
+    path: '/#pg2',
+  },
+  {
+    name: 'Quanto tempo dura a micropigmentação?',
+    path: '/#pg3',
+  },
+]
+
+export default { sections, dropdownServicos, dropdownPerguntas }
