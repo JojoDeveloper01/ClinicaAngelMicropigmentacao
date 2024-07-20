972851957be070454efab1aee227fed032986412
Joseth <107219399+JojoDeveloper01@users.noreply.github.com>
2024-07-20 17:21:57 +0100

    Update cookie.js

--- src/components/Cookies/cookie.js
+++ src/components/Cookies/cookie.js
@@ -1,17 +1,53 @@
-document.addEventListener("astro:page-load", () => {
-  const btnAcceptCookies = document.getElementById('btn-accept-cookies');
-  const noticeCookies = document.getElementById('notice-Cookies');
-  dataLayer = [];
+interface Window {
+  dataLayer: any[];
+}
 
-  if (!localStorage.getItem('cookies-aceptadas')) {
-    noticeCookies.classList.add('active');
-  } else {
-    dataLayer.push({ 'event': 'cookies-aceptadas' });
+document.addEventListener("astro:page-load", () => {
+  const $ = document.getElementById.bind(document);
+  const btnAcceptCookies = $('btn-accept-cookies');
+  const btnRejectCookies = $('btn-reject-cookies');
+  const noticeCookies = $('notice-Cookies');
+  const bgCookie = $('bgCookie');
+
+  window.dataLayer = window.dataLayer || [];
+
+  if (!localStorage.getItem('cookies-rejected') && !localStorage.getItem('cookies-accepted')) {
+    if (noticeCookies && bgCookie) {
+      document.body.classList.add("no-scroll");
+      noticeCookies.classList.add('active');
+      bgCookie.classList.add('active');
+    }
+  } else if (localStorage.getItem('cookies-accepted')) {
+    window.dataLayer.push({ 'event': 'cookies-accepted' });
   }
 
-  btnAcceptCookies.addEventListener('click', () => {
-    noticeCookies.classList.remove('active');
-    localStorage.setItem('cookies-aceptadas', true);
-    dataLayer.push({ 'event': 'cookies-aceptadas' });
-  });
+  function handleCookieAcceptance(isAccepted: boolean) {
+    if (noticeCookies && bgCookie) {
+      noticeCookies.classList.remove('active');
+      bgCookie.classList.remove('active');
+      document.body.classList.remove("no-scroll");
+    }
+
+    if (isAccepted) {
+      localStorage.setItem('cookies-accepted', 'true');
+      localStorage.removeItem('cookies-rejected');
+      window.dataLayer.push({ 'event': 'cookies-accepted' });
+    } else {
+      localStorage.setItem('cookies-rejected', 'true');
+      localStorage.removeItem('cookies-accepted');
+      window.dataLayer.push({ 'event': 'cookies-rejected' });
+    }
+  }
+
+  if (btnAcceptCookies) {
+    btnAcceptCookies.addEventListener('click', () => {
+      handleCookieAcceptance(true);
+    });
+  }
+
+  if (btnRejectCookies) {
+    btnRejectCookies.addEventListener('click', () => {
+      handleCookieAcceptance(false);
+    });
+  }
 });
